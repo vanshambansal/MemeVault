6b314cb9390fc21fd6d132eb20dfdb7082467aa6
Justin <justin7359900@gmail.com>
2024-11-27 17:12:09 -0500

    Setup starter code

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,21 +1,10 @@
 import { ethers } from "ethers"
 
 function Header({ account, setAccount }) {
-  async function connectHandler() {
-    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-    const account = ethers.getAddress(accounts[0])
-    setAccount(account);
-  }
 
   return (
     <header>
-      <p className="brand">fun.pump</p>
 
-      {account ? (
-        <button onClick={connectHandler} className="btn--fancy">[ {account.slice(0, 6) + '...' + account.slice(38, 42)} ]</button>
-      ) : (
-        <button onClick={connectHandler} className="btn--fancy">[ connect ]</button>
-      )}
     </header>
   );
 }
